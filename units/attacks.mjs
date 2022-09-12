842a4aa06120684765c3f9100a571ab820518739
Timothy Means <tbmeans2005@gmail.com>
2022-09-12 15:46:42 -0400

    merged trimjump with attacks test

--- units/attacks.mjs
+++ units/attacks.mjs
@@ -18,6 +18,32 @@ const attackOnWhiteMap = [ 'g2:g3,g4,g5,g6,g7,g8' ].concat(
 );
 
 const listAttacks = {
+  whiteKingSurroundedByBlackInAllDirectionsListsCheckOrPinAllAround() {
+    assertWithErrHandling(
+      expand("4r3/1b6/3n1n2/2nP1pn1/r3K2r/2n2qn1/2bnBn2/4q3"), "w", null,
+      [ 'e5,e6,e7,e8', 'f6', 'f5', 'g5', 'f4,g4,h4', 'g3', 'f3', 'f2',
+      'e2:e3,e1', 'd2', 'd3,c2', 'c3', 'd4,c4,b4,a4', 'c5', 'd5:c6,b7', 'd6']
+    );
+  },
+  whiteKingSurroundedButFlipColorOfAttackersGivesEmptyAttackMap() {
+    assertWithErrHandling(
+      expand("4R3/1B6/3N1N2/2NP1PN1/R3K2R/2N2QN1/2BNPN2/4Q3"), 'w', null,
+      noAttacks
+    )
+  },
+  blackKingSurroundedByWhiteInAllDirectionsListsCheckOrPinAllAround() {
+    assertWithErrHandling(
+      expand("4R3/1B6/3N1N2/2Np1BN1/R3k2R/2NP1QN1/3NbN2/4Q3"), 'b', null,
+      [ 'e5,e6,e7,e8', 'f6', 'f5', 'g5', 'f4,g4,h4', 'g3', 'f3', 'f2',
+      'e2:e3,e1', 'd2', 'd3', 'c3', 'd4,c4,b4,a4', 'c5', 'd5:c6,b7', 'd6']
+    );
+  },
+  blackKingSurroundedButFlipColorOfAttackersGivesEmptyAttackMap() {
+    assertWithErrHandling(
+      expand("4r3/1b6/3n1n2/2np1bn1/r3k2r/2np1qn1/3nbn2/4q3"), 'b', null,
+      noAttacks
+    );
+  },
   evergreenGameMateHasOneCheckByAdjacentBishop() {
     assertWithErrHandling(
       expand("1r3kr1/pbpBBp1p/1b3P2/8/8/2P2q2/P4PPP/3R2K1"), "b", null,
